9b076de968c35d3e9e65550dbe781edaea492157
Alexandre Hardy <hardyalexandre2000@gmail.com>
2024-07-17 20:10:10 +0200

    file setup for prod

---
+++ frontend/setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Clone or update the Flutter repository
+if cd flutter; then
+    git pull && cd ..
+else
+    git clone https://github.com/flutter/flutter.git
+fi
+
+# Set up Flutter environment
+export PATH="$PATH:$(pwd)/flutter/bin"
+echo "PUBLIC_ACCESS_TOKEN_MAPBOX=$PUBLIC_ACCESS_TOKEN_MAPBOX" >> .env
+echo "API_URL=$API_URL" >> .env
+echo "API_URL_ANDROID=$API_URL_ANDROID" >> .env
+echo "SOCKET_IO_URL=$SOCKET_IO_URL" >> .env
+echo "SOCKET_IO_URL_ANDROID=$SOCKET_IO_URL_ANDROID" >> .env
+echo "PUBLIC_API_KEY_STRIPE=$PUBLIC_API_KEY_STRIPE" >> .env
+
+# Build the Flutter web app
+flutter doctor
+flutter pub get
+flutter build web --release --web-renderer html --output build/web
